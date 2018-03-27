da16b0aa7d29e9714aa4a90636f7ee2e7f6d8aad
Richard <richou6@gmail.com>
2018-03-28 00:36:46 +0200

    Add two buttons

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -6,12 +6,49 @@ class HomeWidget extends StatefulWidget {
 }
 
 class HomeWidgetState extends State<HomeWidget> {
+
+  void _onColorPickerSelected() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('RGB Control')
       ),
+      body: new Center(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new InkWell(
+              onTap: () { _onColorPickerSelected(); },
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new Image.asset('images/color-picker.png'),
+                  ),
+                  new Expanded(
+                    child: new RichText(
+                      text: new TextSpan(text: 'Color Picker', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.black87, fontSize: 20.0)),
+                    ),
+                  )
+                ],
+              )
+            ),
+            new Row(children: <Widget>[
+              new Expanded(
+                child: new Image.asset('images/random.png'),
+              ),
+              new Expanded(
+                child: new RichText(
+                  text: new TextSpan(text: 'Random Color', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.black87, fontSize: 20.0)),
+                )
+              )
+            ],)
+          ],
+        ),
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,28 @@ import 'package:flutter/material.dart';
 
 import 'package:rgb_control_app/home_widget.dart';
 
-void main() => runApp(new MyApp());
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
 
-class MyApp extends StatelessWidget {
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+void main() => runApp(new RGBControlApp());
+
+class RGBControlApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Navigation',
+      routes: <String, WidgetBuilder> {
+        '/home': (BuildContext context) => new HomeWidget()
+      },
+      theme: Theme.of(context).platform == TargetPlatform.iOS ? kIOSTheme : kDefaultTheme,
       home: new HomeWidget(),
     );
   }
