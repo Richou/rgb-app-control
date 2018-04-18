ca4b505ade63287e6f97637830375b6cc063ea82
Richard <richou6@gmail.com>
2018-04-18 23:50:35 +0200

    Add Some random pick color

--- lib/random_color_widget.dart
+++ lib/random_color_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RandomColorWidget extends StatefulWidget {
   @override
@@ -6,6 +8,33 @@ class RandomColorWidget extends StatefulWidget {
 }
 
 class RandomColorWidgetState extends State<RandomColorWidget> {
+
+  Color _randomColor;
+  String _rgbString = "N/A";
+
+  @override
+  void initState() {
+    super.initState();
+    _randomColor = Colors.green;
+  }
+
+  _computeRandomColor() {
+    double randomHue = new Random().nextDouble() * 360.0;
+    Color theColor = new HSVColor.fromAHSV(1.0, randomHue, 1.0, 1.0).toColor();
+    setState(() {
+      _randomColor = theColor;
+    });
+    _computeColorStringWithMapping();
+  }
+
+  _computeColorStringWithMapping() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String redBinding = sharedPreferences.getString("RedBinded");
+    setState(() {
+      _rgbString = "$redBinding: ${_randomColor.red}, Green: ${_randomColor.green}, Blue: ${_randomColor.blue}";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -13,8 +42,36 @@ class RandomColorWidgetState extends State<RandomColorWidget> {
       appBar: new AppBar(
         title: new Text('Random Color')
       ),
-      body: new Center(
-        child: new Text('Randomize a color'),
+      body: new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            new Center(
+              child: new Text("Click on the Button bellow to create random color"),
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+                bottom: 16.0
+              ),
+              child: new RaisedButton(
+                child: const Text('RANDOM COLOR'),
+                textColor: Colors.white,
+                color: Colors.blue,
+                onPressed: () {
+                  _computeRandomColor();
+                },
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+                bottom: 16.0
+              ),
+              child: new Text(_rgbString),
+            )
+          ],
+        ),
       )
     );
   }
