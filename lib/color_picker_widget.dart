fa3f3674d4fd5d7e2437876b35869f4b77adabd8
Richard <richou6@gmail.com>
2018-04-27 00:05:53 +0200

    Add color picker

--- lib/color_picker_widget.dart
+++ lib/color_picker_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:rgb_control_app/color_request.dart';
 
 class ColorPickerWidget extends StatefulWidget {
   @override
@@ -6,6 +8,21 @@ class ColorPickerWidget extends StatefulWidget {
 }
 
 class ColorPickerWidgetState extends State<ColorPickerWidget> {
+  Color pickerColor = new Color(0xff443a49);
+  Color currentColor = new Color(0xff443a49);
+
+  ColorRequest colorRequest;
+
+  @override
+  void initState() {
+    super.initState();
+    colorRequest = new ColorRequest();
+  }
+
+  changeColor(Color color) {
+    colorRequest.sendColor(color);
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -14,7 +31,12 @@ class ColorPickerWidgetState extends State<ColorPickerWidget> {
         title: new Text('Color Picker')
       ),
       body: new Center(
-        child: new Text('Pick a color'),
+        child: new ColorPicker(
+          pickerColor: pickerColor,
+          onColorChanged: changeColor,
+          colorPickerWidth: 1000.0,
+          pickerAreaHeightPercent: 0.7,
+        ),
       )
     );
   }
